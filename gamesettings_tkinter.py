147a4eb68fa51b8b7612f9fedae365a9051d4002
Joey <jord.vo@gmail.com>
2021-12-10 19:50:09 -0800

    Starting ading joystick support to the settings tkinter menu

--- gamesettings_tkinter.py
+++ gamesettings_tkinter.py
@@ -1,11 +1,65 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
 
+class ControllerSettingsWindow(tk.Toplevel):
+    def __init__(self, master):
+        super().__init__(master)
+        self.master = master
+        self.create_widgets()
+
+    def create_widgets(self):
+        row = 0
+
+        welcome_message = """
+        Hi there.
+        
+        Please choose how you wish to control the game.
+        """
+
+        message = tk.Label(self, text=welcome_message)
+        message.grid(column=0, row=row)
+        row = row+1
+
+        keyboard_button = tk.Button(self,
+                                    text="Keyboard",
+                                    command=self.apply_keyboard_settings)
+        keyboard_button.grid(column=0, row=row)
+        row = row+1
+
+        joystick_button = tk.Button(self,
+                                    text="Joystick",
+                                    command=self.apply_joystick_settings)
+        joystick_button.grid(column=0, row=row)
+        row = row+1
+
+        return_to_menu_button = tk.Button(self,
+                                        text="Save & Return",
+                                        command=self.save_and_return)
+        return_to_menu_button.grid(column=0, row=row)
+        row = row+1
+
+    def save_and_return(self):
+        print (self.master.settings)
+        self.destroy()
+
+    def apply_keyboard_settings(self):
+        self.master.settings["Controller Preference"] = "Keyboard"
+        message = tk.messagebox.showinfo(title="Controls switched.",
+                                         message="Keyboard controls chosen.")
+        print ("Keyboard chosen!")
+
+    def apply_joystick_settings(self):
+        self.master.settings["Controller Preference"] = "Joystick"
+        message = tk.messagebox.showinfo(title="Controls switched.",
+                                         message="Joystick controls chosen.")
+        print("Joystick chosen!")
+
 class GameSetupApp(tk.Toplevel):
     def __init__(self, master):
         super().__init__(master)
         self.master = master
         self.settings = {
+            "Default Controls"   :   True,
         }
         self.start_game = False
         self.create_widgets()
@@ -45,8 +99,17 @@ class GameSetupApp(tk.Toplevel):
         start_game_button.grid(column=0, row=row)
         row = row+1
 
+        quit_button = tk.Button(self,
+                                    text="Quit Game",
+                                    command=self.do_quit_game)
+        quit_button.grid(column=0, row=row)
+        row = row+1
+
+    def do_quit_game(self):
+        self.quit()
+
     def open_controller_settings(self):
-        pass
+        self.controller_settings_window = ControllerSettingsWindow(self)
 
     def open_video_settings(self):
         pass
