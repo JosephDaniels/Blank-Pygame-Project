3b5e2205d4150691891f97e1edcc896757bd8315
Joey <jord.vo@gmail.com>
2022-01-21 02:09:58 -0800

    I'm doing some testing to try and make collision testing work, it's not ready yet. I gotta do some lookups within the map layer class to see if I hit any of the blocks.

---
+++ maps/dungeon_tiles.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="dungeon_tiles" tilewidth="64" tileheight="64" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="tiles/dungeon_corner.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="tiles/dungeon_wall.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="tiles/dungeon_wall_right.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="tiles/sandy_floor.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="tiles/door_way.png"/>
+ </tile>
+</tileset>
